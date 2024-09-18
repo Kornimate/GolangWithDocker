2a3bca01b20155abffd9a87f2832ba922ef1a071
Kornidesz Máté <dev.profile.km@gmail.com>
2024-09-18 17:02:23 +0200

    Getting started with go

---
+++ go-backend/hello.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var testString string = "Hello World!"
+
+	var testFloat = 0.12
+
+	testInt := 12
+
+	fmt.Println(testString)
+	fmt.Println(testFloat)
+	fmt.Println(testInt)
+}
